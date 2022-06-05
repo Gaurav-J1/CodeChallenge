77b443a03caf46c31bc642e78c954b55382889b3
Gaurav-J1 <60550579+Gaurav-J1@users.noreply.github.com>
2022-06-05 20:18:41 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,32 @@
+import java.util.* ;
+import java.io.*;
+public class Solution {
+    public static void setZeros(int matrix[][]) {
+        int[] r = new int[matrix.length];
+        int[] c = new int[matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    r[i] = 1;
+                    c[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            if (r[i] == 1) {
+                for (int j = 0; j < matrix[0].length; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+
+        }
+        for (int j = 0; j < matrix[0].length; j++) {
+            if (c[j] == 1) {
+                for (int i = 0; i < matrix.length; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
+}
